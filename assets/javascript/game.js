9bcc436f293f9d62131343640a5fb62f2f21b8b7
Marlo Bennett <marlomariebennett@gmail.com>
2016-08-17 01:11:51 -0500

    updated attack function and other variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,7 +19,14 @@ var selectedDefender;
 
 var selectedCharacterHP;
 var selectedCharacterAP;
+var selectedCharacterOriginalAP;
 var selectedCharacterCAP;
+var selectedCharacterName;
+
+var selectedDefenderHP;
+var selectedDefenderAP;
+var selectedDefenderCAP;
+var selectedDefenderName;
 
 var enemyChosen = false;
 
@@ -40,6 +47,7 @@ function initializeNewGame() {
 //SELF: add power values as classes, use parseint to get value later crystal file 12 counter = counter + parseInt($(this).data('num'));
 	imageLeia = $("<div class='charImg charLeia'> <p>" + powers.name[0] + "</p> <img src='assets/images/leia.jpg'> <p>" + powers.healthPoints[0] + "</p> </div>");
 	$("#chooseCharacter").append(imageLeia);
+	$(".charLeia").data("identity", powers.name[0]);
 	$(".charLeia").attr("data-hp", powers.healthPoints[0]);
 	$(".charLeia").attr("data-ap", powers.attackPower[0]);
 	$(".charLeia").attr("data-cap", powers.counterAttackPower[0]);
@@ -48,38 +56,29 @@ function initializeNewGame() {
 
 	imagePadme = $("<div class='charImg charPadme'> <p>" + powers.name[1] + "</p> <img src='assets/images/padme.jpg'> <p>" + powers.healthPoints[1] + "</p> </div>");
 	$("#chooseCharacter").append(imagePadme);
-	$(".charLeia").attr("data-hp", powers.healthPoints[1]);
-	$(".charLeia").attr("data-ap", powers.attackPower[1]);
-	$(".charLeia").attr("data-cap", powers.counterAttackPower[1]);
+	$(".charPadme").data("identity", powers.name[1]);
+	$(".charPadme").attr("data-hp", powers.healthPoints[1]);
+	$(".charPadme").attr("data-ap", powers.attackPower[1]);
+	$(".charPadme").attr("data-cap", powers.counterAttackPower[1]);
 
 	imageRey = $("<div class='charImg charRey'> <p>" + powers.name[2] + "</p> <img src='assets/images/rey.jpg'> <p>" + powers.healthPoints[2] + "</p> </div>");
 	$("#chooseCharacter").append(imageRey);
-	$(".charLeia").attr("data-hp", powers.healthPoints[2]);
-	$(".charLeia").attr("data-ap", powers.attackPower[2]);
-	$(".charLeia").attr("data-cap", powers.counterAttackPower[2]);
+	$(".charRey").data("identity", powers.name[2]);
+	$(".charRey").attr("data-hp", powers.healthPoints[2]);
+	$(".charRey").attr("data-ap", powers.attackPower[2]);
+	$(".charRey").attr("data-cap", powers.counterAttackPower[2]);
 
 	imageZam = $("<div class='charImg charZam'> <p>" + powers.name[3] + "</p> <img src='assets/images/zam.jpg'> <p>" + powers.healthPoints[3] + "</p> </div>");
 	$("#chooseCharacter").append(imageZam);
-	$(".charLeia").attr("data-hp", powers.healthPoints[3]);
-	$(".charLeia").attr("data-ap", powers.attackPower[3]);
-	$(".charLeia").attr("data-cap", powers.counterAttackPower[3]);
+	$(".charZam").data("identity", powers.name[3]);
+	$(".charZam").attr("data-hp", powers.healthPoints[3]);
+	$(".charZam").attr("data-ap", powers.attackPower[3]);
+	$(".charZam").attr("data-cap", powers.counterAttackPower[3]);
 
 //reset powers to original values
-/*	leiaPowers.healthPoints = 150;
-	leiaPowers.attackPower = 10;
-	leiaPowers.counterAttackPower = 20;
-
-	padmePowers.healthPoints = 90;
-	padmePowers.attackPower = 8;
-	padmePowers.counterAttackPower = 35;
-
-	reyPowers.healthPoints = 135;
-	reyPowers.attackPower = 12;
-	reyPowers.counterAttackPower = 15;
-
-	zamPowers.healthPoints = 85;
-	zamPowers.attackPower = 14;
-	zamPowers.counterAttackPower = 30;*/
+	powers.healthPoints = [130, 110, 135, 100];
+	powers.attackPower = [10, 13, 12, 14];
+	powers.counterAttackPower = [20, 25, 15, 30];
 
 	selectCharacter();
 	battle();
@@ -93,20 +92,31 @@ function selectCharacter() {
 		//moves your selection to the your character section if it's empty
 			$(this).appendTo("#yourCharacter");
 			selectedCharacter = this;
+			//capturues your character's stats
+			selectedCharacterName = $(this).data("identity");
 			selectedCharacterHP = $(this).data("hp");
 			selectedCharacterAP = $(this).data("ap");
+			selectedCharacterOriginalAP = $(this).data("ap");
 			selectedCharacterCAP = $(this).data("cap");
 			console.log(selectedCharacter);
-			console.log(selectedCharacterHP);
-			console.log(selectedCharacterAP);
-			console.log(selectedCharacterCAP);
+			console.log("selected char hp " + selectedCharacterHP);
+			console.log("selected char ap " + selectedCharacterAP);
+			console.log( "selected car cap " + selectedCharacterCAP);
 		}
 		else {
 			//moves your selection to defender if you've already chosen your character
 			$(".noDefender").remove();
 			$(this).appendTo("#defenders");
+			//captures defender's stats
 			selectedDefender = this;
+			selectedDefenderName = $(this).data("identity");
+			selectedDefenderHP = $(this).data("hp");
+			selectedDefenderAP = $(this).data("ap");
+			selectedDefenderCAP = $(this).data("cap");
 			console.log(selectedDefender);
+			console.log("selected defender hp " + selectedDefenderHP);
+			console.log("selected defender AP " + selectedDefenderAP);
+			console.log("selected defender cap " + selectedDefenderCAP);
 			enemyChosen = true;
 		}
 		//moves other characters to the enemies section
@@ -123,14 +133,43 @@ function selectCharacter() {
 function battle() {
 
 	$("#fight").on("click", function(event) {
+		
+		//var firstFight = true;
+
 		//warns if no enemy has been selected
 		if (!enemyChosen) {
 			$("<h5 class='noDefender'>There is no enemy to attack.</h5>").appendTo("#defenders");
 		}
-		
+		else {
+			$(".attackReport").remove();
+			
+			//Your HP goes down by defender's CAP damage
+			selectedCharacterHP = (selectedCharacterHP - selectedDefenderCAP);
+			
+			//defender's HP goes down by your AP
+			selectedDefenderHP = (selectedDefenderHP - selectedCharacterAP);
+			
+			//you attacked defender for your AP damage
+			$("<h5 class='attackReport'>You attacked " + selectedDefenderName + " for " + selectedCharacterAP + " damage.</h5>").appendTo("#defenders");
+
+			//defender attacked you for her CAP damage
+			$("<h5 class='attackReport'>" + selectedDefenderName + " attacked you back for " + selectedDefenderCAP + " damage.</h5>").appendTo("#defenders");
+			console.log("selected char hp " + selectedCharacterHP);
+
+			//increases your character's AP
+			selectedCharacterAP = (selectedCharacterAP + selectedCharacterOriginalAP);
+			console.log("selected char ap " + selectedCharacterAP);
+			console.log("selected defender hp " + selectedDefenderHP);
+		}
 
 	});
 }
+
+
+
+
+
+//your AP increases by your AP
 
 
 //should increase your attack power by its base power (show text message with HP) and decrease his HP by your current attack power (number by his images lowers accordingly)
